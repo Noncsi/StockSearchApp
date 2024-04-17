65b68e2b1a3222bda66df7a46869b1c03acf6742
Noncsi <lengyel.noemi96@gmail.com>
2024-04-17 03:00:29 +0200

    add mockdata in case of no response + basic styling on detail page

--- src/app/detail/[symbol]/page.tsx
+++ src/app/detail/[symbol]/page.tsx
@@ -39,23 +39,47 @@ type StockPrice = {
   volume: number;
 };
 
+const mockFetchData = {
+  Information: "",
+  "Meta Data": {
+    "1. Information":
+      "Intraday (5min) open, high, low, close prices and volume",
+    "2. Symbol": "IBM",
+    "3. Last Refreshed": "2024-04-16 19:55:00",
+    "4. Interval": "5min",
+    "5. Output Size": "Compact",
+    "6. Time Zone": "US/Eastern",
+  },
+  "Time Series (5min)": {
+    "2024-04-16 19:55:00": {
+      "1. open": "183.9900",
+      "2. high": "183.9900",
+      "3. low": "183.7600",
+      "4. close": "183.7600",
+      "5. volume": "16",
+    },
+  },
+};
+
 export default async function Page({
   params: { symbol },
 }: {
   params: { symbol: string };
 }) {
   const res = await fetch(
-    `${API_BASE_URL}/query?function=${QUERY_FUNCTION_TIME_SERIES}&symbol=${symbol}&interval=${QUERY_INTERVAL}&apikey=${QUERY_API_KEY}`
+    `${API_BASE_URL}/query?function=${QUERY_FUNCTION_TIME_SERIES}&symbol=${symbol}&interval=${QUERY_INTERVAL}&apikey=${QUERY_API_KEY}`,
+    { cache: "force-cache" }
   );
 
   if (!res.ok) {
     throw new Error("Failed to fetch data.");
   }
 
-  const fetchResponse: FetchResponse = await res.json();
+  let fetchResponse: FetchResponse = await res.json();
 
   if (Object.keys(fetchResponse.Information.length)) {
-    throw new Error("Failed to fetch data due to API request limitation.");
+    console.error("Failed to fetch data due to API request limitation.");
+    fetchResponse = mockFetchData;
   }
 
   const timestampKeys = Object.keys(fetchResponse["Time Series (5min)"]);
@@ -81,33 +105,46 @@ export default async function Page({
   };
 
   return (
-    <main>
-      <h1 className="text-thd-color-violet-90 thd-max-xl thd-heading-lg mb-md">
-        {stockData.symbol}
-      </h1>
-      <div className="text-slate-500">
-        {stockData.lastRefreshed.toDateString()}
-      </div>
-      <div key={stockData.latestPrice.dateTime.getMilliseconds()}>
-        <div>
-          <div className="text-slate-500">closing price: </div>
-          <div className="text-xl font-medium">
-            ${stockData.latestPrice.close}
+    <div className="py-8 lg:py-16 flex items-center px-6">
+      <div className="text-center mx-auto inline-block">
+        <div className="mb-2">
+          <h1 className="text-3xl lg:text-6xl leading-tight max-w-3xl font-bold tracking-tight mt-6 mx-auto">
+            {stockData.symbol}
+          </h1>
+          <div className="text-slate-500">
+            {stockData.lastRefreshed.toDateString()}
           </div>
-          <div className="text-slate-500">highest price: </div>
-          <div className="text-xl font-medium">
-            ${stockData.latestPrice.high}
+        </div>
+        <div
+          key={stockData.latestPrice.dateTime.getMilliseconds()}
+          className="flex flex-row flex-wrap justify-center gap-6"
+        >
+          <div>
+            <div className="text-slate-500">Closing price</div>
+            <div className="text-xl font-medium">
+              ${stockData.latestPrice.close}
+            </div>
           </div>
-          <div className="text-slate-500">lowest price: </div>
-          <div className="text-xl font-medium">
-            ${stockData.latestPrice.low}
+          <div>
+            <div className="text-slate-500">Highest price</div>
+            <div className="text-xl font-medium">
+              ${stockData.latestPrice.high}
+            </div>
           </div>
-          <div className="text-slate-500">volume: </div>
-          <div className="text-xl font-medium">
-            {stockData.latestPrice.volume}
+          <div>
+            <div className="text-slate-500">Lowest price</div>
+            <div className="text-xl font-medium">
+              ${stockData.latestPrice.low}
+            </div>
+          </div>
+          <div>
+            <div className="text-slate-500">Volume</div>
+            <div className="text-xl font-medium">
+              {stockData.latestPrice.volume}
+            </div>
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
